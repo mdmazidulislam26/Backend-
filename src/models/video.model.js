1a1bdd8c555b2d91eca73f5a63b1e4280e6d3777
Md Mazidul Islam <mazid9326@gmail.com>
2025-02-14 01:04:29 +0600

    User and video model with hooks and JWT

---
+++ src/models/video.model.js
@@ -0,0 +1,49 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate
+ from "mongoose-aggregate-paginate-v2";
+
+
+const videoSchema = new Schema (
+    {
+        videoFile : {
+            type : String, //cloudinary url
+            required : true
+        },
+        thumbnail : {
+            type : String, //cloudinary url
+            required : true
+        },
+        title : {
+            type : String, 
+            required : true
+        },
+        description : {
+            type : String, 
+        },
+        duration : {
+            type : Number, //cloudinary url
+            required : true
+        },
+        views : {
+            type : Number,
+            required : 0
+        },
+        isPublished : {
+            type : Boolean, 
+            required : true
+        },
+        owner : {
+            type : Schema.Types.ObjectId,
+            ref : "User"
+        },
+    },
+    {
+        timestamps : true
+    }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model('Video',videoSchema);
+
+//! npm i mongoose-aggregate-paginate-v2
